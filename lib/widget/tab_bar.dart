379a79ae3335e7045f58b67592202c54a01de45c
makoha innocent <vantagepoint2019@outlook.com>
2024-02-09 00:36:09 +0300

    added scrolling to tab bar

--- lib/widget/tab_bar.dart
+++ lib/widget/tab_bar.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/widget/stack.dart';
 import 'package:flutter_application_1/widget/table.dart';
@@ -14,27 +13,33 @@ class _TabBarSampleState extends State<TabBarSample> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("tab bar demo"),
-          bottom: TabBar(
-        tabs: [
-          Tab(icon: Icon(Icons.flight)),
-          Tab(icon: Icon(Icons.directions_transit)),
-          Tab(icon: Icon(Icons.directions_car)),
-        ],
-      ),
-        ),
-        body: const Center(
-          child: TabBarView(
-      children: [
-        Icon(Icons.flight, size: 350),
-        Icon(Icons.directions_transit, size: 350),
-        Icon(Icons.directions_car, size: 350),
-      ],
-        )
-      ),
-    ));
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("tab bar demo"),
+            bottom: TabBar(
+              isScrollable: true,
+              indicatorColor: Colors.amber,
+              //indicatorSize: TabBarIndicatorSize.label,
+              //indicatorWeight: 10,
+              /*indicator: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50), // Creates border
+                  color: Colors.greenAccent),*/
+              tabs: [
+                Tab(icon: Icon(Icons.flight)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_car)),
+              ],
+            ),
+          ),
+          body: const Center(
+              child: TabBarView(
+            children: [
+              Icon(Icons.flight, size: 350),
+              Icon(Icons.directions_transit, size: 350),
+              Icon(Icons.directions_car, size: 350),
+            ],
+          )),
+        ));
   }
 }
